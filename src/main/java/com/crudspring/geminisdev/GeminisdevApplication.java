db1ec34f80b7eae4841e691838f7bb724bb14192
EncodeGeminis Developer <kimerastream@gmail.com>
2024-09-07 21:50:29 -0600

    Primer Commit al repositorio

---
+++ src/main/java/com/crudspring/geminisdev/GeminisdevApplication.java
@@ -0,0 +1,13 @@
+package com.crudspring.geminisdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminisdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminisdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudspring/geminisdev/GeminisdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudspring.geminisdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminisdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
