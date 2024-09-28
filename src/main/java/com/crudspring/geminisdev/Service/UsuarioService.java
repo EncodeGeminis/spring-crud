1f9be545f2c581b7306c4006b6e3b4aecfcd649f
EncodeGeminis Developer <kimerastream@gmail.com>
2024-09-28 10:12:23 -0600

    se agrego la paginacion en la tabla de usuarios

--- src/main/java/com/crudspring/geminisdev/Service/UsuarioService.java
+++ src/main/java/com/crudspring/geminisdev/Service/UsuarioService.java
@@ -2,6 +2,8 @@ package com.crudspring.geminisdev.Service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.crudspring.geminisdev.Entity.Usuario;
 
@@ -10,12 +12,16 @@ public interface UsuarioService {
     public void eliminarUsuario(Long id);
     public Usuario buscarUsuario(Usuario usuario);
     public Usuario buscarUsuarioPorId(Long id);
-    public List<Usuario> listarUsuarios();
+    //public List<Usuario> listarUsuarios();
+    public Page<Usuario> listarUsuarios(Pageable pageable);
+    public Page<Usuario> buscarUsuarioPorNombre(String nombre, Pageable pageable);
+    public Page<Usuario> buscarPorNombreParcial(String nombre, Pageable pageable);
+
     public Usuario buscarPorNombre(String nombre);
     public Usuario obtenerUsuarioPorId(Long id);
     public void actualizarUsuario(Usuario usuario);
     //metodos personalizados
     public List<Usuario> buscarListaNombres(String nombre);
-    public List<Usuario> buscarPorNombreParcial(String nombre);
+    //public List<Usuario> buscarPorNombreParcial(String nombre);
 
 }
